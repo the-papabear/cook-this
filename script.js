5910890a3d77241a950400e2af206129cc63aa5d
gabriel <rgabrielursu@gmail.com>
2021-04-20 23:50:57 +0300

    managed to get data from an API

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ const listedIngredients = document.querySelector('.selected-ingredients');
 
 const selectedIngredientsArr = [];
 
-console.log(selectedIngredientsArr);
-
-//Add user ingredients input to empty array
-ingredientSearch.addEventListener('click', function(e){
+let clickAdd = ingredientSearch.addEventListener('click', function(e){
     e.preventDefault();
+    console.log('Button clicked.');
     let ingredientValue = ingredientSearchBar.value;
-    console.log(typeof(ingredientValue));
-
     selectedIngredientsArr.push(ingredientValue);
-    console.log(selectedIngredientsArr);
-
     
     listedIngredients.insertAdjacentHTML('beforeend', `<i> ${ingredientValue} </i>`)
 });
 
-fetch('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3');
+async function recipeAPI(url){
+        await fetch(url).then((response) => response.json()).then((data) => renderRecipe(data));
+}
+
+function renderRecipe(data){
+    let img = document.getElementById('recipe-photo').src = data.hits[0].recipe.image;
+}
+
+recipeAPI(`https://api.edamam.com/search?q=chicken&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450`);
