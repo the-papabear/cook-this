cb76743d3d3a8667f4735afcc2eee8704325dadd
gabriel <rgabrielursu@gmail.com>
2021-04-21 23:54:29 +0300

    added the randomness part of the app

--- script.js
+++ script.js
@@ -1,61 +1,37 @@
 const ingredientSearchBar = document.getElementById("ingredients");
 const listedIngredients = document.querySelector(".selected-ingredients");
 const selectedIngredientsArr = [];
-
-//Display ingredients input on the page:
-//   document.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     if (!e.target.matches("#ingredient-submit")) return;
-
-//     console.log("Button clicked.");
-//     let ingredientValue = ingredientSearchBar.value;
-//     selectedIngredientsArr.push(ingredientValue);
-
-//     listedIngredients.insertAdjacentHTML(
-//       "beforeend",
-//       `<i> ${ingredientValue} </i>`
-//     );
-
-//     async function recipeAPI(ingredients) {
-//       await fetch(
-//         `https://api.edamam.com/search?q=${ingredients}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450 `
-//       )
-//         .then((response) => response.json())
-//         .then((data) => renderRecipe(data));
-//     }
-//   });
-// };
+let ingredientValue;
 
 document.addEventListener("click", function (e) {
   if (!e.target.matches("#ingredient-submit")) return;
 
-  let ingredientValue = ingredientSearchBar.value;
+  ingredientValue = ingredientSearchBar.value;
   selectedIngredientsArr.push(ingredientValue);
 
   listedIngredients.insertAdjacentHTML(
     "beforeend",
     `<i> ${ingredientValue} </i>`
   );
+});
+
+document.addEventListener("click", function (e) {
+  if (!e.target.matches("#recipe-search")) return;
 
   fetch(
-    `https://api.edamam.com/search?q=${ingredientValue}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450 `
+    `https://api.edamam.com/search?q=${ingredientValue}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450&to=30`
   )
     .then((response) => response.json())
     .then((data) => renderRecipe(data));
 });
 
-//Fetch data from url and render the data on the page
-// async function recipeAPI(ingredients) {
-//   await fetch(
-//     `https://api.edamam.com/search?q=${ingredients}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450 `
-//   )
-//     .then((response) => response.json())
-//     .then((data) => renderRecipe(data));
-// }
-
 function renderRecipe(data) {
   let ingredients = (document.querySelector(
     ".recipe__ingredients"
-  ).textContent = data.hits[0].recipe.ingredientLines);
+  ).textContent = data.hits[getRandom(0, 30)].recipe.label);
   console.log(ingredients);
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
