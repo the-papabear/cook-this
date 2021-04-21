0083d1871047ce74b255f241c0bf1b896c3952cd
gabriel <rgabrielursu@gmail.com>
2021-04-21 18:10:07 +0300

    linked input to api fetch

--- script.js
+++ script.js
@@ -3,30 +3,55 @@ const listedIngredients = document.querySelector(".selected-ingredients");
 const selectedIngredientsArr = [];
 
 //Display ingredients input on the page:
-let clickAdd = function (ingredients) {
-  document.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (!e.target.matches("#ingredient-submit")) return;
+//   document.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     if (!e.target.matches("#ingredient-submit")) return;
 
-    console.log("Button clicked.");
-    let ingredientValue = ingredientSearchBar.value;
-    selectedIngredientsArr.push(ingredientValue);
+//     console.log("Button clicked.");
+//     let ingredientValue = ingredientSearchBar.value;
+//     selectedIngredientsArr.push(ingredientValue);
 
-    listedIngredients.insertAdjacentHTML(
-      "beforeend",
-      `<i> ${ingredientValue} </i>`
-    );
-  });
-};
+//     listedIngredients.insertAdjacentHTML(
+//       "beforeend",
+//       `<i> ${ingredientValue} </i>`
+//     );
 
-//Fetch data from url and render the data on the page
-async function recipeAPI(ingredients) {
-  await fetch(
-    `https://api.edamam.com/search?q=${ingredients}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450 `
+//     async function recipeAPI(ingredients) {
+//       await fetch(
+//         `https://api.edamam.com/search?q=${ingredients}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450 `
+//       )
+//         .then((response) => response.json())
+//         .then((data) => renderRecipe(data));
+//     }
+//   });
+// };
+
+document.addEventListener("click", function (e) {
+  if (!e.target.matches("#ingredient-submit")) return;
+
+  let ingredientValue = ingredientSearchBar.value;
+  selectedIngredientsArr.push(ingredientValue);
+
+  listedIngredients.insertAdjacentHTML(
+    "beforeend",
+    `<i> ${ingredientValue} </i>`
+  );
+
+  fetch(
+    `https://api.edamam.com/search?q=${ingredientValue}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450 `
   )
     .then((response) => response.json())
     .then((data) => renderRecipe(data));
-}
+});
+
+//Fetch data from url and render the data on the page
+// async function recipeAPI(ingredients) {
+//   await fetch(
+//     `https://api.edamam.com/search?q=${ingredients}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450 `
+//   )
+//     .then((response) => response.json())
+//     .then((data) => renderRecipe(data));
+// }
 
 function renderRecipe(data) {
   let ingredients = (document.querySelector(
@@ -34,5 +59,3 @@ function renderRecipe(data) {
   ).textContent = data.hits[0].recipe.ingredientLines);
   console.log(ingredients);
 }
-
-console.log(selectedIngredientsArr);
