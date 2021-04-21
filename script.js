799afa8e652220c9f5a3ea1d27cdaccafc4a790e
the-papabear <rgabrielursu@gmail.com>
2021-04-21 17:09:47 +0200

    grinding through it

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-const ingredientSearch = document.getElementById('ingredient-submit');
-const ingredientSearchBar = document.getElementById('ingredients');
-const listedIngredients = document.querySelector('.selected-ingredients');
-
+const ingredientSearchBar = document.getElementById("ingredients");
+const listedIngredients = document.querySelector(".selected-ingredients");
 const selectedIngredientsArr = [];
 
-let clickAdd = ingredientSearch.addEventListener('click', function(e){
+//Display ingredients input on the page:
+let clickAdd = function (ingredients) {
+  document.addEventListener("click", function (e) {
     e.preventDefault();
-    console.log('Button clicked.');
+    if (!e.target.matches("#ingredient-submit")) return;
+
+    console.log("Button clicked.");
     let ingredientValue = ingredientSearchBar.value;
     selectedIngredientsArr.push(ingredientValue);
-    
-    listedIngredients.insertAdjacentHTML('beforeend', `<i> ${ingredientValue} </i>`)
-});
 
-async function recipeAPI(url){
-        await fetch(url).then((response) => response.json()).then((data) => renderRecipe(data));
+    listedIngredients.insertAdjacentHTML(
+      "beforeend",
+      `<i> ${ingredientValue} </i>`
+    );
+  });
+};
+
+//Fetch data from url and render the data on the page
+async function recipeAPI(ingredients) {
+  await fetch(
+    `https://api.edamam.com/search?q=${ingredients}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450 `
+  )
+    .then((response) => response.json())
+    .then((data) => renderRecipe(data));
 }
 
-function renderRecipe(data){
-    let img = document.getElementById('recipe-photo').src = data.hits[0].recipe.image;
+function renderRecipe(data) {
+  let ingredients = (document.querySelector(
+    ".recipe__ingredients"
+  ).textContent = data.hits[0].recipe.ingredientLines);
+  console.log(ingredients);
 }
 
-recipeAPI(`https://api.edamam.com/search?q=chicken&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450`);
+console.log(selectedIngredientsArr);
