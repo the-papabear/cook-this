11692329b7a80fa45377d77f636943612de230e1
gabriel <rgabrielursu@gmail.com>
2021-04-23 18:51:44 +0300

    more javascript functionality that works

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ const ingredientSearchBar = document.getElementById("ingredients");
 const listedIngredients = document.querySelector(".selected-ingredients");
 const selectedIngredientsArr = [];
 let ingredientValue;
+let recipeNumber;
 
+//Fetch Data from the API:
+function getData(recipeNumber) {
+  if (recipeNumber === 30) {
+    fetch(
+      `https://api.edamam.com/search?q=${selectedIngredientsArr}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450&to=${recipeNumber}`
+    )
+      .then((response) => response.json())
+      .then((data) => renderRecipe(data));
+  } else {
+    fetch(
+      `https://api.edamam.com/search?q=${selectedIngredientsArr}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450&to=${recipeNumber}`
+    )
+      .then((response) => response.json())
+      .then((data) => renderRecipeList(data));
+  }
+}
+
+//Show ingredients on the website on button click:
 document.addEventListener("click", function (e) {
   if (!e.target.matches("#ingredient-submit")) return;
 
   ingredientValue = ingredientSearchBar.value;
   selectedIngredientsArr.push(ingredientValue);
+
+  console.log(selectedIngredientsArr);
 
   listedIngredients.insertAdjacentHTML(
     "beforeend",
@@ -15,23 +36,41 @@ document.addEventListener("click", function (e) {
   );
 });
 
+//Gives a random recipe:
 document.addEventListener("click", function (e) {
-  if (!e.target.matches("#recipe-search")) return;
+  if (!e.target.matches("#recipe-random")) return;
 
-  fetch(
-    `https://api.edamam.com/search?q=${ingredientValue}&app_id=509fe698&app_key=fef25a374d1aad8e5497a7bc0aa92450&to=30`
-  )
-    .then((response) => response.json())
-    .then((data) => renderRecipe(data));
+  getData(30);
+});
+
+//Gives a list of 10 recipes with a hyperlink:
+document.addEventListener("click", function (e) {
+  if (!e.target.matches("#recipe-list")) return;
+
+  getData(10);
 });
 
 function renderRecipe(data) {
-  let ingredients = (document.querySelector(
-    ".recipe__ingredients"
-  ).textContent = data.hits[getRandom(0, 30)].recipe.label);
-  console.log(ingredients);
+  let ingredients = data.hits[getRandom()].recipe;
+  let ingredientsTest = data;
+  console.log(ingredientsTest);
+
+  document.querySelector(".recipe__title").textContent = ingredients.label;
+
+  document.querySelector(".recipe__ingredients").textContent =
+    ingredients.ingredientLines;
+
+  document.getElementById("recipe-photo").src = ingredients.image;
+
+  document.querySelector(".tutorial__source").href = ingredients.url;
 }
 
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+function renderRecipeList(data) {
+  let recipeList = data.hits.forEach(function render(recipe) {
+    document.querySelector(".tutorial__source").href = recipe.url;
+  });
+}
+
+function getRandom() {
+  return Math.floor(Math.random());
 }
